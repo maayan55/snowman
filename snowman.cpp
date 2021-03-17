2373b4e463b9b2d0cd98df367da18f32470e14a9
maayan55 <47734711+maayan55@users.noreply.github.com>
2021-03-17 08:39:59 +0200

    Add files via upload

--- snowman.cpp
+++ snowman.cpp
@@ -1,57 +1,54 @@
 #include "snowman.hpp"
 
-/* this file contains one big main function that build a snowman.
-the function get number with 8 digits, all between 1-4
-and make a snow man.*/
 
 const int MIN_VALID_NUM = 11111111;
 const int MAX_VALID_NUM = 44444444;
 const int MIN_LEN = 10000000;
 const int MAX_LEN = 99999999;
 
-    string base(int x) {
+    string base(int x) { //options for the base of the snowman
         if(x == 1) {return " ( : )\n";}
         if(x == 2) {return " (\" \")\n";}
         if(x == 3) {return " (___)\n";}
         if(x == 4) {return " (   )\n";}
         throw std::out_of_range{"invalid digit"};
     }
-    string torso(int x) {
+    string torso(int x) { //options for the torso of the snowman
         if(x == 1) {return "( : )";} 
         if(x == 2) {return "(] [)";}
         if(x == 3) {return "(> <)";}
         if(x == 4) {return "(   )";}
         throw std::out_of_range{"invalid digit"};
     }
-    string right_arm(int x) {
+    string right_arm(int x) { //options for the right arm of the snowman
         if(x == 1) {return ">";}
         if(x == 2) {return "/";}
         if(x == 3) {return "\\";}
         if(x == 4){return " ";}
         throw std::out_of_range{"invalid digit"};
     }
-    string left_arm(int x) {
+    string left_arm(int x) { //options for the left arm of the snowman
         if(x == 1) {return "<";}
         if(x == 2) {return "\\";}
         if(x == 3) {return "/";}
         if(x == 4){return " ";}
         throw std::out_of_range{"invalid digit"};
     }
-    string eye(int x) {
+    string eye(int x) { //options for the eye of the snowman
         if(x == 1) {return ".";}
         if(x == 2) {return "o";}
         if(x == 3) {return "O";}
         if(x == 4) {return "-";}
         throw std::out_of_range{"invalid digit"};
     }
-    string nose_mouth(int x) {
+    string nose_mouth(int x) { //options for the nose/mouth of the snowman
         if(x == 1) {return ",";}
         if(x == 2) {return ".";}
         if(x == 3) {return "_";}
         if(x == 4) {return " ";}
         throw std::out_of_range{"invalid digit"};
     }
-    string hat(int x) {
+    string hat(int x) { //options for the hat of the snowman
         if(x == 1) {return  " _===_\n";}
         if(x == 2) {return "  ___\n .....\n";}
         if(x == 3) {return "   _\n  /_\\\n";}
@@ -62,15 +59,16 @@ const int MAX_LEN = 99999999;
 
 namespace ariel{
     string snowman(int num){
-        if(num<MIN_LEN || num > MAX_LEN){
+        if(num<MIN_LEN || num > MAX_LEN) { //if the number is too short/ long (not 8 digit)
             throw std::length_error{"the input should have exactly 8 digits, so " + to_string(num) + " invalid"};
         }
-        if(num > MAX_VALID_NUM || num < MIN_VALID_NUM){
+        if(num > MAX_VALID_NUM || num < MIN_VALID_NUM) { //if the digits of the  number are not in the right range
             throw std::out_of_range{"the input should be between 11111111 to 44444444, SO "+ to_string(num) + "invalid"};
         }
-        if(num < 0){
-            throw std::domain_error{"the input should be positive, so " + to_string(num) + " invalid"};
+        if(num < 0) { //if the number is negetive
+            throw std::domain_error{"the input cant be negetive, so " + to_string(num) + " invalid"};
         }
+        
         int B = num %10;
         num /= 10;
         int T = num %10;
@@ -87,21 +85,21 @@ namespace ariel{
         num /= 10;
         int H = num %10; 
 
-        string t1;
+        string middle;
         string face = "(" + eye(L) + nose_mouth(N) + eye(R) + ")";
         if(Y == 2 && X == 2) {
-            t1 = left_arm(X) + face + right_arm(Y) + "\n " + torso(T) + " \n";
-        }
-        else if(X == 2) {
-            t1 = left_arm(X) + face + " \n " + torso(T) + right_arm(Y) + "\n";
+            middle = left_arm(X) + face + right_arm(Y) + "\n " + torso(T) + " \n";
         }
         else if(Y == 2) {
-            t1 = " " +  face + right_arm(Y) + "\n" + left_arm(X) + torso(T) + " \n";
+            middle = " " +  face + right_arm(Y) + "\n" + left_arm(X) + torso(T) + " \n";
+        }
+        else if(X == 2) {
+            middle = left_arm(X) + face + " \n " + torso(T) + right_arm(Y) + "\n";
         }
         else {
-            t1 = " " +  face + " \n" + left_arm(X) + torso(T) + right_arm(Y) + "\n";
+            middle = " " +  face + " \n" + left_arm(X) + torso(T) + right_arm(Y) + "\n";
         }
-        string ans = hat(H) + t1 + base(B);
+        string ans = hat(H) + middle + base(B);
         return ans; 
     }
 }
